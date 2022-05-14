aca85f99db3f8d7ac0133c6d2e2ecf8a45640e8b
JS @breaktoprotect <breaktoprotect@gmail.com>
2022-05-14 22:02:54 +0800

    Added more stuff

--- yaml_test/read_yaml.py
+++ yaml_test/read_yaml.py
@@ -6,8 +6,24 @@ What    : Test yaml in python. Run the code to see what your YAML is like in Pyt
 import yaml
 
 #* Read YAML file
-with open("example.yaml", 'rb') as yaml_file:
+with open("example.yaml", 'r') as yaml_file:
     example = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
     print("'Example' object type:", type(example))
     print("Print 'example' yaml object as is: ", example)
+
+#separator
+print("---")
+print("")
+
+# Opening single file with multiple YAML defined
+with open("two_yaml_in_one.yaml", 'r') as yaml_file:
+    example = yaml.load_all(yaml_file) # This will create a gen since multiple YAML documents
+
+    print("two_yaml_in_one.yml:")
+    for index,obj in enumerate(example):
+        print("Object",index)
+        print("obj type:", type(obj))
+        print("Content:", obj)
+        print("")
+
